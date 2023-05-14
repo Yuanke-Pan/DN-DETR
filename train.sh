18f4621e5071f758e7c24533df6e9cf517c6edb6
Yuanke Pan <7107708338@qq.com>
2023-05-14 13:29:42 +0800

    add train script

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
   python main.py -m dn_dab_yolo_deformable_detr \
   --output_dir logs/dn_yolo_DABDETR/R50 \
   --batch_size 4 \
-  --epochs 50 \
+  --epochs 72 \
   --lr_drop 40 \
-  --coco_path /data1/yuanke/data/coco \
-  --use_dn
+  --coco_path /public/home/yuanke/data/coco \
+  --use_dn \
+  --resume /public/home/yuanke/project/DN-DETR/logs/dn_yolo_DABDETR/R50/checkpoint0049.pth
--- train_ditribute.sh
+++ train_ditribute.sh
@@ -1,8 +1,9 @@
-python -m torch.distributed.launch --nproc_per_node=8 \
+python -m torch.distributed.launch --nproc_per_node=6 \
   main.py -m dn_dab_yolo_deformable_detr \
   --output_dir logs/dn_yolo_DABDETR/R50 \
-  --batch_size 4 \
-  --epochs 50 \
+  --batch_size 5 \
+  --epochs 72 \
   --lr_drop 40 \
-  --coco_path /data1/yuanke/data/coco \
-  --use_dn
+  --coco_path /public/home/yuanke/data/coco/ \
+  --use_dn \
+  --resume /public/home/yuanke/project/DN-DETR/logs/dn_yolo_DABDETR/R50/checkpoint0049.pth
